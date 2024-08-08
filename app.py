5b6bed0658872a4e57f09ac613e673cc573c9ff1
Josue <josue.medina9991@gmail.com>
2024-08-07 21:08:09 -0600

    deployment

--- app.py
+++ app.py
@@ -134,6 +134,6 @@ async def chatbox_endpoint(websocket: WebSocket):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000) 
+    uvicorn.run(app, host="0.0.0.0", port=8000) 
 
 
